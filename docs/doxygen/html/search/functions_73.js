c122c9b4840342ab9a8c7599d5155b97af5391ef
Florian Richoux <florian.richoux@gmail.com>
2013-09-29 17:17:40 +0200

    Finished to doxygened all files.

--- docs/doxygen/html/search/functions_73.js
+++ docs/doxygen/html/search/functions_73.js
@@ -1,7 +1,9 @@
 var searchData=
 [
   ['sameteam',['sameTeam',['../classGame.html#a37a035ff6df37f80453e657306ca1137',1,'Game']]],
+  ['setcard',['setCard',['../classTrick.html#a4d2b4c09c8d7c255ff34bde5fe29532a',1,'Trick']]],
   ['setdifficulty',['setDifficulty',['../classAI.html#a19cae044bb7f221819f61b017b6a7ffe',1,'AI']]],
+  ['showallcards',['showAllCards',['../classTrick.html#a24eae8424ae5666d362dc0a9e514faa4',1,'Trick']]],
   ['showcards',['showCards',['../classPlayer.html#a3813d858078112fa9118b20ed4794e65',1,'Player']]],
   ['showdeck',['showDeck',['../classGame.html#ae53579403950f816d662454021f6d460',1,'Game']]],
   ['showplayerscards',['showPlayersCards',['../classGame.html#a280383798308721f9655dbf509dbc050',1,'Game']]],
