b6be861b61d9be56ca76de6b41c576350affd461
richoux <florian.richoux@gmail.com>
2013-12-16 22:13:09 +0100

    Indenting properly (get rid of gnu emacs style)

--- include/Deck.hpp
+++ include/Deck.hpp
@@ -1,23 +1,23 @@
 /*
-* Tarot is an application for Android system to play to French Tarot.
-* Please visit https://github.com/richoux/Tarot for further information.
-* 
-* Copyright (C) 2013 Florian Richoux
-*
-* This file is part of Tarot.
-* Tarot is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
+ * Tarot is an application for Android system to play to French Tarot.
+ * Please visit https://github.com/richoux/Tarot for further information.
+ * 
+ * Copyright (C) 2013 Florian Richoux
+ *
+ * This file is part of Tarot.
+ * Tarot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
 
-* Tarot is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
+ * Tarot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
 
-* You should have received a copy of the GNU General Public License
-* along with Tarot.  If not, see http://www.gnu.org/licenses/.
-*/
+ * You should have received a copy of the GNU General Public License
+ * along with Tarot.  If not, see http://www.gnu.org/licenses/.
+ */
 
 #pragma once
 
@@ -44,14 +44,14 @@ public:
   /*!
     \param card A pointer of Card.
     \return True iff the given card is in the deck.
-   */
+  */
   bool isInDeck( shared_ptr<Card> card );
 
   //! Tests if the current deck has a stronger card than to proposed one.
   /*!
     \param card A pointer of Card.
     \return True iff the deck has a stronger card than the input. 
-   */
+  */
   bool hasStrongerThan( shared_ptr<Card> card );
 
   //! Make a new deal; create each card of the game.
