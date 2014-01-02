13d33fa8b3b2cfc6a4d10d07a23ec5da1dd6d30b
Florian Richoux <florian.richoux@polytechnique.edu>
2014-01-02 11:29:54 +0100

    Update doxygen: delete rule of 3 for AI/Player

--- docs/doxygen/html/search/all_6f.js
+++ docs/doxygen/html/search/all_6f.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['operator_3c',['operator<',['../classCard.html#a0210e809d9ec749824716ec1f5ebbf8a',1,'Card::operator&lt;()'],['../classTeam.html#a1811d6b7e660181d8209b455cb9d92e0',1,'Team::operator&lt;()']]],
   ['operator_3c_3c',['operator<<',['../classCard.html#a19eb35e24a8f6368e34575698fca9008',1,'Card::operator&lt;&lt;()'],['../classTeam.html#a6a20eb26a63a224b3970386af40bfb2b',1,'Team::operator&lt;&lt;()']]],
-  ['operator_3d',['operator=',['../classAI.html#a1eb14f1ec8ef778f070362ba37f17d6c',1,'AI']]],
   ['operator_3d_3d',['operator==',['../classCard.html#a54a1553ca40e32768ee09465447b8090',1,'Card']]],
   ['operator_3e',['operator>',['../classCard.html#a50bd8a1a5d5bdfce5727718844f87ce8',1,'Card::operator&gt;()'],['../classTeam.html#a3164302f1fd5d70db2e85994488f8054',1,'Team::operator&gt;()']]],
   ['opponentshavesuit',['opponentsHaveSuit',['../classAI.html#affad35d086fe81539b07a5626b19c1bb',1,'AI']]]
--- docs/doxygen/html/search/functions_61.js
+++ docs/doxygen/html/search/functions_61.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['addcard',['addCard',['../classPlayer.html#a7c7b8325247c91ef255ca493de9f35eb',1,'Player']]],
-  ['ai',['AI',['../classAI.html#a059fcf3ea09f908bca75567937f7e8a7',1,'AI::AI(const string &amp;name, const vector&lt; string &gt; &amp;knownPartners)'],['../classAI.html#aa58c0b5165f06063316d257abc996e41',1,'AI::AI(const AI &amp;other)'],['../classAI.html#a8a2a39a4e227c23829cb77aa0f362af3',1,'AI::AI(AI &amp;&amp;other)']]],
+  ['ai',['AI',['../classAI.html#a059fcf3ea09f908bca75567937f7e8a7',1,'AI']]],
   ['ascalledking',['asCalledKing',['../classTrick.html#a4d196a8097feccd87f1aca65ea4e80c8',1,'Trick']]]
 ];
