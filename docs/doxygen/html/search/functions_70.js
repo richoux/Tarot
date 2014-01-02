dd11b0d6485301c5858eaed36f60178d5e4d9d4e
richoux <florian.richoux@polytechnique.edu>
2014-01-02 11:29:54 +0100

    Update doxygen: delete rule of 3 for AI/Player

--- docs/doxygen/html/search/functions_70.js
+++ docs/doxygen/html/search/functions_70.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['play',['play',['../classGame.html#a6a76e181e24425eb361960d5b5d184bd',1,'Game']]],
   ['playcard',['playCard',['../classAI.html#a3f5b2888e03634db701c66f82a4f03a8',1,'AI::playCard()'],['../classBeginner.html#a73e2bf8d807166c994c42fd3b6bfc7bf',1,'Beginner::playCard()'],['../classHuman.html#a3258d3ce0eec7a5e393c639506ef28e5',1,'Human::playCard()'],['../classPlayer.html#aeba090a124bfd9a3666d2d793439cae0',1,'Player::playCard()'],['../classStratDiff.html#a550903bf95e6a897f346debab972f33a',1,'StratDiff::playCard()']]],
-  ['player',['Player',['../classPlayer.html#a5a2790fccf10c30106370d910c96d317',1,'Player::Player(const string &amp;name)'],['../classPlayer.html#a4a8b7f689e924b15a9e6f9888983fb43',1,'Player::Player(const Player &amp;other)'],['../classPlayer.html#ae3d3eb16a37c49cf91b5093632061d69',1,'Player::Player(Player &amp;&amp;other)']]],
+  ['player',['Player',['../classPlayer.html#a5a2790fccf10c30106370d910c96d317',1,'Player']]],
   ['printscores',['printScores',['../classGame.html#acb39f55a7b0b1d0d35b06d305a508dd4',1,'Game']]]
 ];
