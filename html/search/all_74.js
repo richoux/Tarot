dfe195add9111fa83e1665b3bdfceae6d765d7b3
Florian Richoux <florian.richoux@gmail.com>
2014-02-07 17:14:23 +0100

    add doxygen files

---
+++ html/search/all_74.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['takebiddings',['takeBiddings',['../classGame.html#ae8240e18a7b5ed1adec7663278afc256',1,'Game']]],
+  ['takedog',['takeDog',['../classGame.html#ab725d07327db4dfb836e90eb08c83393',1,'Game']]],
+  ['team',['Team',['../classTeam.html',1,'']]],
+  ['trick',['Trick',['../classTrick.html',1,'Trick'],['../classTrick.html#a2d52be4c67d65793531d2dc96122ae72',1,'Trick::Trick()']]],
+  ['trumps',['trumps',['../classPlayer.html#a66b3455fc8c6685a78eb193a015a07a0',1,'Player']]]
+];
---
+++ html/search/classes_74.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['team',['Team',['../classTeam.html',1,'']]],
+  ['trick',['Trick',['../classTrick.html',1,'']]]
+];
