3a91d3c51a39b51505399c318333eb20d1b4981c
Florian Richoux <florian.richoux@polytechnique.edu>
2015-12-27 15:11:57 +0100

    Update doxy: 5-player game card call

--- html/search/all_7.js
+++ html/search/all_7.js
@@ -1,11 +1,11 @@
 var searchData=
 [
+  ['indexindeck',['indexInDeck',['../classDeck.html#afe7b0b266ec9a30c271333d00a3b4168',1,'Deck']]],
   ['initialcards',['initialCards',['../classPlayer.html#aac0d154d58a07310ee23c3c763f2cfa9',1,'Player']]],
   ['initialpoints',['initialPoints',['../classPlayer.html#af625acf7f8ad267630c7e3bd50d2683b',1,'Player']]],
   ['iscomparable',['isComparable',['../classCard.html#a2c531d32de5e76e758254b9d03689ae5',1,'Card']]],
   ['isfacecard',['isFaceCard',['../classCard.html#ac8978fa4de9e59246381ebd2f9c5ecfc',1,'Card']]],
   ['isfool',['isFool',['../classCard.html#aa05732992ee8352a205a15d89f4edda4',1,'Card']]],
-  ['isindeck',['isInDeck',['../classDeck.html#aaafdac9dd57ada20cc78dd330c741963',1,'Deck']]],
   ['isopponent',['isOpponent',['../classAI.html#aec78eb8010bdcd99c899aaa7af79d9ea',1,'AI']]],
   ['isoudler',['isOudler',['../classCard.html#a8ac4b04ac18721afdff8d504a7621a97',1,'Card']]],
   ['ispartner',['isPartner',['../classAI.html#a0cd8c8e4a646c6c1ae51c7812eb1194e',1,'AI']]],
--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,6 +1,8 @@
 var searchData=
 [
+  ['callablecards',['callableCards',['../classPlayer.html#a2880662cb5f5cc2e778242c7a1477b30',1,'Player']]],
   ['card',['Card',['../classCard.html#a113a46b46e64efe0fd98a42df301db9a',1,'Card']]],
+  ['chooseking',['chooseKing',['../classAI.html#a6d7eafe5efa20fd78ec91048ad7c1ae2',1,'AI::chooseKing()'],['../classGame.html#a68d2c56aaaa1b3d2c01bc9968f0166b2',1,'Game::chooseKing()'],['../classHuman.html#a7b465bef30158c2b5552f54651e48318',1,'Human::chooseKing()'],['../classPlayer.html#ac85ce39f638b28f13588e8957dc952ff',1,'Player::chooseKing()']]],
   ['computescore',['computeScore',['../classGame.html#af3ae0695c2600b273e14d9fa93dd980a',1,'Game']]],
   ['contains',['contains',['../classTeam.html#ae3bc7f55a2204019bf393ccd110ce873',1,'Team']]]
 ];
