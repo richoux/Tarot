f6c9e7600413674fdcf83cf041658ce6d6fb8c33
Florian Richoux <florian.richoux@polytechnique.edu>
2015-12-31 06:04:19 +0100

    Change memory_check to recompile first

--- memory_check.sh
+++ memory_check.sh
@@ -1,2 +1,4 @@
-valgrind --leak-check=full --show-reachable=yes bin/tarot --auto
-valgrind --tool=cachegrind bin/tarot --auto
+make clean
+make debug
+valgrind --leak-check=full --show-reachable=yes bin/tarot --debug
+valgrind --tool=cachegrind bin/tarot --debug
