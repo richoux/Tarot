5cfbc480d4b0c8cd90e2688fbf43292b04e21367
Florian Richoux <florian.richoux@polytechnique.edu>
2016-01-05 12:35:09 +0100

    Copy/paste main-nogui into main

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,125 @@
 
 using namespace std;
 
+void gameLoop( Game &game )
+{
+  game.dealCards();
+  
+  if( game.isBotsOnly() )
+    game.showPlayersCards();
+
+  bool allPassed = true;
+  Biddings bestBid = Biddings::none;  
+  for( int p = 0 ; p < game.getNumberPlayers() ; ++p )
+  {
+    auto bid = game.takeBiddings( bestBid );
+    if( bid.second == Biddings::none )
+      cout << bid.first->name << " passes." << endl;
+    else
+    {
+      allPassed = false;
+      if( bestBid < bid.second )
+      {
+	game.updateIndexBidder();
+	bestBid = bid.second;
+      }
+      
+      cout << bid.first->name << " takes a ";
+      switch( bid.second )
+      {
+      case Biddings::small:
+	cout << "Small." << endl;
+	break;
+      case Biddings::guard:
+	cout << "Guard." << endl;
+	break;
+      case Biddings::guard_w:
+	cout << "Guard Without." << endl;
+	break;
+      case Biddings::guard_a:
+	cout << "Guard Against." << endl;
+	break;
+      default:
+	break;
+      }
+    }
+  }
+    
+  if( allPassed )
+  {
+    cout << "All players passed." << endl;
+    return;
+  }
+
+  game.closeBiddings();
+  
+  if( game.getNumberPlayers() == 5 )
+  {
+    game.chooseKing();
+    cout << game.getTakers().members.begin()->first << " called " << *game.getKingCalled() << endl;
+  }
+  
+  if( game.getBidding() <= Biddings::guard )
+    printDog( game );
+    
+  game.takeDog();
+  if( game.isKingFound() && game.isBotsOnly() )
+  {
+    cout << "Player alone! Unlucky!" << endl
+	 << "Taker: " << game.getTakers() << endl
+	 << "Defenders: " << game.getDefenders() << endl;      
+  }
+  
+  cout << "Taker: " << game.getTakers() << endl;
+  if( game.getNumberPlayers() <= 5 )
+    cout << "Defenders: " << game.getDefenders() << endl;
+
+  if( game.isBotsOnly() )
+    game.showPlayersCards();
+
+  bool kingJustFound;
+  
+  for( int round = 0; round < game.getCardsPerPlayer(); ++round )
+  {
+    string roundString("Round ");
+    roundString += to_string( round + 1 );
+    printRound( roundString );
+
+    auto trick = game.playTrick( kingJustFound );
+    cout << "Trick: ";
+    trick->showAllCards();
+    cout << "=> Won by " << trick->getLeader()->name << endl;
+
+    if( kingJustFound && game.isBotsOnly() )
+    {
+      cout << "Parter known!" << endl
+	   << "Taker: " << game.getTakers() << endl
+	   << "Defenders: " << game.getDefenders() << endl;      
+    }
+
+  }
+
+  Team winners = game.endGame();
+
+  cout << "Won cards:" << endl;
+  for( auto player : game.getPlayers() )
+  {
+    cout << player->name << ": ";
+    for( auto card : game.getPlayerWonCards( player ) )
+      cout << *card << " ";
+    cout << endl;
+  }
+
+  if( game.getBidding() > Biddings::guard )
+    printDog( game );
+
+  if( !winners.isEmpty() )
+  {
+    game.printScores();  
+    cout << "Winners: " << winners << endl;
+  }
+}
+
 int main( int argc, char **argv )
 {
   QApplication app(argc, argv);
@@ -45,37 +164,34 @@ int main( int argc, char **argv )
   Game *game;
   string playerName;
   int nberPlayers;
-  int loop;
+  int loop = 1;
 
-  QGraphicsView view(&scene);
-  view.setWindowTitle("Mari's Tarot");
-  view.show();
-
-  if( argc >= 2 && strcmp( argv[1], "--debug") == 0 )
+  if( argc >= 2 )
   {
+    string arg( argv[1] );
+    if( arg.compare( "--debug" ) != 0 || argc > 4 )
+    {
+      usage();
+      exit(1);
+    }
+    
     if( argc == 2 )
-    {
       nberPlayers = 4;
-      loop = 1;
-    }
     else if( argc == 3 )
-    {
       nberPlayers = atoi( argv[2] );
-      loop = 1;
-    }
     else
     {
       nberPlayers = atoi( argv[2] );
       loop = atoi( argv[3] );
     }
 
-    game = new Game( nberPlayers, playerName, true );
+    game.setGame( nberPlayers, playerName, true );
 
-    game->shuffleDeck();
-    game->showDeck();
-    game->showPlayersCards();
+    game.shuffleDeck();
+    game.showDeck();
+    game.showPlayersCards();
   }
-  else if( argc == 1 )
+  else
   {
     cout << "Please enter your name." << endl;
     getline( cin, playerName );
@@ -83,44 +199,26 @@ int main( int argc, char **argv )
       nberPlayers = getInt( "Please enter the number of players: 3, 4 or 5.\n" );
     } while( nberPlayers < 3 || nberPlayers > 5 );
     
-    if( playerName.compare("") != 0 )
-      game = new Game( nberPlayers, playerName );
+    if( !playerName.empty() )
+      game.setGame( nberPlayers, playerName );
     else
-      game = new Game( nberPlayers );
+      game.setGame( nberPlayers );
 
-    game->shuffleDeck();
+    game.shuffleDeck();
   }
 
-  if( argc >= 2 && strcmp( argv[1], "--debug") == 0 )
+  if( game.isBotsOnly() )
   {
     for( int i = 0 ; i < loop ; ++i )
     {
-      cout << "***********" << endl;
-      cout << "*** " << i << " ***" << endl;
-      cout << "***********" << endl;
-
-      Team winners = game->play();
-
-      if( !winners.isEmpty() )
-      {
-	game->printScores();  
-	cout << "Winners: " << winners << endl;
-      }
+      string numberGames( to_string( i ) );
+      printRound( numberGames );
       
-      game->newGame();
+      gameLoop( game );
+      game.newGame();
     }
   }
   else
-  {
-    Team winners = game->play();
-    
-    if( !winners.isEmpty() )
-    {
-      game->printScores();  
-      cout << "Winners: " << winners << endl;
-    }
-  }
-
+    gameLoop( game );
   return app.exec();
-  // delete game;
 }
